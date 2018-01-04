dcc3a9fe6b9990a6242c3486b724010d1fda2f22
macDaai <546201056@qq.com>
2018-01-04 11:59:41 +0800

    retrofit 简洁封 第一步
    初步的模仿了一下别人的，后续继续完善

---
+++ app/src/main/java/com/ziyouzhuhe/zhuanqianbao/utils/LogUtil.java
@@ -0,0 +1,15 @@
+package com.ziyouzhuhe.zhuanqianbao.utils;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by sjj on 2018/1/4.
+ */
+
+public class LogUtil {
+    public static  void e(String m){
+        Log.e(TAG, "e: "+m);
+    }
+}
